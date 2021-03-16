32a63f2724f8fcb4caf32e812a23d4adfbc892db
Andreas Bradahl <andreas.bradahl@avella.no>
2021-03-16 09:17:41 +0100

    add initial script file

---
+++ buildbar.py
@@ -0,0 +1,18 @@
+import os, sys
+
+# Store root folder name
+root_app_name = os.path.basename(os.getcwd())
+
+# Store all folder names in 'projects' in list
+dirs = next(os.walk("projects"))[1]
+
+# Concatenate all 'projects' folders to string
+folders = ' '.join(dirs)
+
+env = {sys.argv[1]}
+
+command = 'cmd /k "ace & mqsipackagebar -w projects -a ' + root_app_name + '.' + env + '.bar -k ' + folders + '"'
+
+print(command)
+
+os.system(command)

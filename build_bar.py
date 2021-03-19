6e83b651542fc9cd53cf9ac1cc3897db4702123b
Andreas Bradahl <andreas.bradahl@avella.no>
2021-03-19 11:26:59 +0100

    refactor script name

---
+++ build_bar.py
@@ -0,0 +1,38 @@
+import os, sys
+from pathlib import Path
+
+# Store root folder name
+root = Path.cwd()
+git_repo_name = Path.cwd().name
+
+# Store all folder names in 'projects' in list and concat to string
+# dirs = next(os.walk("projects"))[1]
+folders = [folder for folder in root.glob('projects/*') if folder.is_dir()]
+folders_string = ' '.join(folder.name for folder in folders)
+
+print("Folders string: " + folders_string)
+
+# Get environment from CLI argument
+valid_envs = ['readyapi', 'local', 'test', 'qa', 'prod']
+if len(sys.argv) >= 2:
+    env_arg = sys.argv[1]
+else:
+    env_arg = input(f'Choose one of the following environments ({valid_envs}): ')
+
+if env_arg in valid_envs:
+    bar_filename = f'{git_repo_name}.{env_arg}.bar'
+    override_file = f'config/{env_arg}/baroverides/{git_repo_name}.{env_arg}.properties'
+
+    command = (
+        f'cmd /k "'
+        f'ace & '
+        f'mqsipackagebar -w projects -a {bar_filename} -k {folders_string} & '
+        f'mqsiapplybaroverride -b {bar_filename} -p {override_file} -k {folders_string} & '
+        f'move {bar_filename} config/{env_arg}/bars'
+        f'"'
+    )
+else:
+    print(f'Invalid argument "{env_arg}" - exiting script...')
+    sys.exit()
+
+os.system(command)
